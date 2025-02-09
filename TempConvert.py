a9e91638627e7536d2b533a4aea68070f4040c1e
cjanes-uno <cjanes@unomaha.edu>
2025-02-09 21:29:20 +0000

    finished TempCovert

--- TempConvert.py
+++ TempConvert.py
@@ -1,17 +1,19 @@
 #TempConvert.py
-#Name:
-#Date:
-#Assignment:
+#Name: Colton Janes
+#Date: 02/09/2025
+#Assignment: Lab 3 - TempConvert
 
 
 def main():
   #Prompt the user for a Fahrenheit temperature
+  tempF = float(input("Provide a temperature, assuming it's in Fahrenheit: "))
+
   #Convert that temperature to celsius, rounding to 1 decimal percision
+  tempC = (tempF - 32) * 5/9
+  tempC = round(tempC, 2)
+
   #Output converted temperature.
-  tempF = 80
+  print(tempF, "degrees Fahrenheit is", tempC, "degrees Celsius.")
 
-  tempC = tempF / 2
-
-  print(tempF, "is ", tempC, "degrees celsius.")
 if __name__ == '__main__':
   main()
